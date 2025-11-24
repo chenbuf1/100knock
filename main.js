058df86f0e0c89cbbc18f68ef52a3ece18a85bfd
senkei. <cescr912@gmail.com>
2025-11-25 00:09:56 +0900

    Update main.js 31

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("Hello, JavaScript!");
+
+// 変数の定義
+let message = "こんにちは、JavaScript！";
+
+// 配列の定義
+let fruits = ["りんご", "バナナ", "みかん"];
+
+// オブジェクトの定義
+let student = {
+  name: "Bufan Chen",
+  age: 25,
+};
+
+// コンソール出力
+console.log("変数：", message);
+console.log("配列：", fruits);
+console.log("オブジェクト：", student);
