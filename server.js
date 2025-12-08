93c16faed03cb3daadbc82521f04676a79121b34
senkei. <cescr912@gmail.com>
2025-12-08 23:07:05 +0900

    42 codeup

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 41：根路径，返回文本
 app.get('/', (req, res) => {
   res.send('Hello, API!');
+});
+
+// 42：/api/greet 路径，返回JSON
+app.get('/api/greet', (req, res) => {
+  res.json({ message: 'Hello!' });
 });
 
 app.listen(port, () => {
