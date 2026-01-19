76238e3128e255f5f8ee46ad771500eea0b1799e
senkei. <cescr912@gmail.com>
2026-01-19 23:37:30 +0900

    Create FruitList.js

---
+++ react_profile/src/FruitList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function FruitList() {
+  const fruits = ['りんご', 'バナナ', 'みかん', 'ぶどう'];
+
+  return (
+    <div>
+      <h2>果物リスト</h2>
+      <ul>
+        {fruits.map((fruit, index) => (  //map() を使ってリストを動的に表示している
+          <li key={index}>{fruit}</li>
+        ))}  //key 属性は index で設定
+      </ul>
+    </div>
+  );
+}
+
+export default FruitList;
