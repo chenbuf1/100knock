3809b8b2daa9e9e2b60c6784f76a9d25e32b264d
senkei. <cescr912@gmail.com>
2026-01-19 23:19:33 +0900

    Update App.js

--- react_profile/src/App.js
+++ react_profile/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import Profile from './Profile';
+import Kid from './kid_72';
 
+/* 71
 function App() {
   return (
     <div className="App">
       <h1>Reactへようこそ</h1>
       <Profile />
+    </div>
+  );
+}
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <h1>自己紹介アプリ</h1>
+      <Profile name="Bufan" age={25} />
     </div>
   );
 }
