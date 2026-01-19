5ad0a88fac3fbb45755f6c4e65f4b3a284d2b357
senkei. <cescr912@gmail.com>
2026-01-19 22:58:27 +0900

    Create Profile.js

---
+++ react_profile/src/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h2>私のプロフィール</h2>
+      <p>名前：陳 卜凡</p>
+      <p>出身：中国</p>
+      <p>趣味：コーヒー・音楽</p>
+    </div>
+  );
+}
+
+export default Profile;
