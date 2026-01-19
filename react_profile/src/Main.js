61a9a8e04e514cbfe048190180a810ef0cc3c19d
senkei. <cescr912@gmail.com>
2026-01-20 00:48:46 +0900

    Create Main.js

---
+++ react_profile/src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main style={{ padding: '20px' }}>
+      <p>これはメインコンテンツです。Reactコンポーネントを使って画面を構成しています。</p>
+    </main>
+  );
+}
+
+export default Main;
