7507c613e1d9f8cbf455786997935917bc58363d
senkei. <cescr912@gmail.com>
2026-01-20 00:49:04 +0900

    Create Footer.js

---
+++ react_profile/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#ddd', padding: '10px', marginTop: '20px' }}>
+      <p>&copy; 2026 Cesc Works</p>
+    </footer>
+  );
+}
+
+export default Footer;
