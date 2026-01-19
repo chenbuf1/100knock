f348c0589b2b3005fbc5fe7aa6562110b9121efd
senkei. <cescr912@gmail.com>
2026-01-20 00:48:33 +0900

    Create Header.js

---
+++ react_profile/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#f0f0f0', padding: '10px' }}>
+      <h1>My React サイト</h1>
+    </header>
+  );
+}
+
+export default Header;
