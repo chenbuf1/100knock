13d32df2c4f5d09c72e822a48ecd8b6007a641fc
senkei. <cescr912@gmail.com>
2026-01-26 18:56:12 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -3,3 +3,12 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   .then((data) => {
     console.log(data);
   });
+
+// 82
+const ul = document.getElementById("postList");
+data.forEach((post) => {
+const li = document.createElement("li");
+li.textContent = post.title;
+ul.appendChild(li);
+});
+});
