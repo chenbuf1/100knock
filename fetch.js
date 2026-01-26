0416ff7d58ece3f18e197d5087b81bb7b1f90e38
senkei. <cescr912@gmail.com>
2026-01-26 19:20:14 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,11 +1,15 @@
+// 82
+const status = document.getElementById("status");
+const ul = document.getElementById("postList");
+
+// 取得中
+status.textContent = "Loading...";
 fetch("https://jsonplaceholder.typicode.com/posts")
 .then((response) => response.json())
 .then((data) => {
-console.log(data); // 80
-
-
-// 82
-const ul = document.getElementById("postList");
+// console.log(data); // 80 
+// 完了後：Loading を消す
+status.textContent = "";
 
 data.forEach((post) => {
 const li = document.createElement("li");
@@ -13,3 +17,11 @@ li.textContent = post.title;
 ul.appendChild(li);
 });
 });
+
+
+
+
+
+
+
+
