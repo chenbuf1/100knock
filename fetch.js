67b322ff09243ce15360e5137e1c79a66f0ced21
senkei. <cescr912@gmail.com>
2026-01-26 20:11:11 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -2,6 +2,7 @@
 const status = document.getElementById("status");
 const ul = document.getElementById("postList");
 
+/* 83
 // 取得中
 status.textContent = "Loading...";
 fetch("https://jsonplaceholder.typicode.com/posts")
@@ -19,8 +20,34 @@ ul.appendChild(li);
 });
 }, 2000); // 2000ms = 2秒
 });
+*/
 
 
+// 取得中
+status.textContent = "Loading...";
+
+// make mistake on URL
+fetch("https://jsonplaceholder.typicode.com/POSTS_WRONG")
+.then((response) => {
+if (!response.ok) {
+throw new Error("API error");
+}
+return response.json();
+})
+.then((data) => {
+// 成功
+status.textContent = "";
+data.forEach((post) => {
+const li = document.createElement("li");
+li.textContent = post.title;
+ul.appendChild(li);
+});
+})
+
+.catch((error) => {
+// 👇 84
+status.textContent = "データの取得に失敗しました";
+});
 
 
 
