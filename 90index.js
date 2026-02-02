13a1e658e0caf50e81138f004fe219a55e14c1dd
senkei. <cescr912@gmail.com>
2026-02-03 00:31:17 +0900

    Update 90index.js

--- 90index.js
+++ 90index.js
@@ -3,14 +3,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// root
 app.get("/", (req, res) => {
   // res.send("Hello, Render!");
   res.send("Hello, Render! Auto Deploy OK ");
 });
-
+// 94 Env variables
 app.get("/env", (req, res) => {
   res.send(`MY_MESSAGE = ${process.env.MY_MESSAGE}`);
 });
+
+
+// /api/hello
+app.get("/api/hello", (req, res) => {
+  res.json({
+    message: "Hello from /api/hello",
+  });
+});
+
+// /about
+app.get("/about", (req, res) => {
+  res.send("This is the about page");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
